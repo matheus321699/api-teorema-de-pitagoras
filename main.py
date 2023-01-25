260de2497016844727d3560e9504bc26ad67856f
matheus321699 <71594615+matheus321699@users.noreply.github.com>
2023-01-25 19:25:38 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+import pandas as pd
+from flask import Flask, make_response, jsonify, request
+import json
+
+app = Flask(__name__)
+
+
+# Classe Triângulo
+class Triangulo:
+
+  def toJson(self):
+    return json.dumps(self,
+                      default=lambda o: o.__dict__,
+                      sort_keys=True,
+                      indent=4)
+
+
+# contruir as funcionalidades
+@app.route('/')
+def homepage():
+  return 'A API está no ar'
+
+
+@app.route('/calculo', methods=['POST'])
+def calculoLadoTriangulo():
+  triangulo = request.json
+
+  triangulo_obj = Triangulo()
+  triangulo_obj.cateto1 = triangulo['cateto1']
+  triangulo_obj.cateto2 = triangulo['cateto2']
+  triangulo_obj.hipotenusa = triangulo['hipotenusa']
+
+  if (triangulo_obj.cateto1 == None):
+    triangulo_obj.cateto1 = pow(
+      ((triangulo_obj.hipotenusa * triangulo_obj.hipotenusa) -
+       (triangulo_obj.cateto2 * triangulo_obj.cateto2)), 1/2)
+
+  if (triangulo_obj.cateto2 == None):
+    triangulo_obj.cateto2 = pow(
+      ((triangulo_obj.hipotenusa * triangulo_obj.hipotenusa) -
+       (triangulo_obj.cateto1 * triangulo_obj.cateto1)), 1/2)
+
+  if (triangulo_obj.hipotenusa == None):
+    triangulo_obj.hipotenusa = pow(
+      ((triangulo_obj.cateto2 * triangulo_obj.cateto2) +
+       (triangulo_obj.cateto1 * triangulo_obj.cateto1)), 1/2)
+
+  triangulo = triangulo_obj.toJson()
+  return triangulo
+
+
+# Rodar API
+app.run(host='0.0.0.0')
